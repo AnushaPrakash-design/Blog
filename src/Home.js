9198561ea125a8fa38c59c81054c3007237fd596
Anusha <anusha.prapad@gmail.com>
2022-08-12 23:36:21 +0530

    feat: Blogs added

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,35 @@
 import { useState } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "How to make chocolate chip cookies",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: "Anusha",
+      id: 1,
+    },
+    {
+      title: "How to make oatmeal cookies",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: "Hari",
+      id: 2,
+    },
+    {
+      title: "How to make macaron cookies",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      author: "Vandana",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2> Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2> {blog.title}</h2>
+          <p>Written by {blog.author} </p>
+        </div>
+      ))}
     </div>
   );
 };
