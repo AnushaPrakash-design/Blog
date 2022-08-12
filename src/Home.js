f10d438a5e2ab8f88cbd663fa1b96f6e0887bd4c
Anusha <anusha.prapad@gmail.com>
2022-08-12 23:13:31 +0530

    feat: learning usesate

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Home = () => {
   return (
     <div className="home">
@@ -7,3 +9,35 @@ const Home = () => {
 };
 
 export default Home;
+
+{
+  /* Click events */
+}
+{
+  /* <button onClick={handleClick()}>Click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("Anusha");
+        }}
+      >
+        Click again
+      </button> */
+}
+
+//  Usestate
+// // const handleClickAgain = (name) => {
+// //   console.log("hello " + name);
+// // };
+
+// // let name = "Anusha";
+// const [name, setName] = useState("Anusha");
+// const [age, setAge] = useState(25);
+
+// const handleClick = () => {
+//   setName("CookiePi");
+//   setAge(1);
+// };
+//  {/* <p>
+//       {name} is {age} years old
+//     </p>
+/* <button onClick={handleClick}>Click me</button> */
